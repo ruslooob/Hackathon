f908ecf5318f27268c863080b19a58760d164cf2
dimasic25 <dimonbubyr@mail.ru>
2021-05-21 16:50:39 +0300

    add task scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Console\Commands\ImportCategoriesCommand;
+use App\Console\Commands\ImportPostersCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,7 +26,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command(ImportPostersCommand::class)->everyMinute();
+        $schedule->command(ImportCategoriesCommand::class)->everyMinute();
     }
 
     /**
