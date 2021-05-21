1f4a2068bdff7919aafe647cd54a772e90e090a0
dimasic25 <dimonbubyr@mail.ru>
2021-05-21 12:19:17 +0300

    add console command ImportDataCommand and GuzzleClient

---
+++ app/Console/Commands/ImportDataCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Components\ImportDataClient;
+use Illuminate\Console\Command;
+
+
+class ImportDataCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:data';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get data from back-poster.admlr.lipetsk.ru';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $import = new ImportDataClient();
+        $response = $import->client->request('GET', 'posters/160');
+        dd($response->getBody()->getContents());
+    }
+}
